d0a03226fcfd1887dfa07db12fc56428f4fe0e40
eliel lima <eliellima2@gmail.com>
2024-07-18 11:45:52 -0300

    atualizacao18/07

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -6,6 +6,12 @@ import Image from "../assets/Group 53660.png";
 import imgCard1 from "../assets/card1.png";
 import imgCard2 from "../assets/card2.png";
 import imgCard3 from "../assets/card3.png";
+import { IoShirtOutline } from "react-icons/io5";
+import { PiBaseballCap, PiSneakerMoveLight, PiPants } from "react-icons/pi";
+import { LuHeadphones } from "react-icons/lu";
+
+
+
 
 
 
@@ -52,10 +58,10 @@ function Home() {
                                     Supreme</span>
                                 <button type="button">Comprar</button>
                             </div>
-                             
+
                             <img src={imgCard1} />
 
-                              
+
 
                         </div>
 
@@ -87,9 +93,54 @@ function Home() {
                     </div>
                 </section>
 
+                <section id="componet3-home">
+                    <h2>Coleções em destaque</h2>
+
+                    <div className="tipo-colecao">
+                        <div className="colecao">
+                            <div className="borda">
+                                <IoShirtOutline className="icon-colecao" />
+                            </div>
+                            <span>Camisetas</span>
+                        </div>
+
+                        <div className="colecao">
+                            <div className="borda">
+                                <PiPants className="icon-colecao" />
+                            </div>
+                            <span>Calças</span>
+                        </div>
+
+                        <div className="colecao">
+                            <div className="borda">
+                                <PiBaseballCap className="icon-colecao" />
+
+                            </div>
+                            <span>Bonés</span>
+                        </div>
+
+                        <div className="colecao">
+                            <div className="borda">
+                                <LuHeadphones className="icon-colecao" />
+                            </div>
+                            <span>Headphones</span>
+                        </div>
+
+                        <div className="colecao">
+
+                            <div className="borda" >
+                                <PiSneakerMoveLight className="icon-colecao" />
+                            </div>
+                            <span>Tênis</span>
+                        </div>
+
+                    </div>
+
+                </section>
+
             </div>
 
-            {/* <Footer /> */}
+            <Footer />
 
 
         </>
