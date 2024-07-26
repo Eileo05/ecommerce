2bd68d7c0576ce7c09c455d3fce8e7812b818631
arthurdemacedo <arthurms1708@gmail.com>
2024-07-26 08:19:11 -0300

    Adiciona Login Page

--- src/PaginaLogin/PaginaLogin.jsx
+++ src/PaginaLogin/PaginaLogin.jsx
@@ -2,6 +2,8 @@ import "../PaginaLogin/PaginaLogin.css";
 import Image from "../assets/logo.png";
 import Image2 from "../assets/sapatos.png";
 import Footer from "../footer/index"
+import Image3 from "../assets/gmailicon.jpeg"
+import Image4 from "../assets/fbicon.png"
 
 
 
@@ -17,15 +19,38 @@ function PaginaLogin() {
 
             <div className="container2">
                 <div className="inputs">
-                    <h1>Acesse sua conta</h1>
-                    <p>Novo cliente? Então registre-se <a href="">aqui</a></p>
-                    <label htmlFor="">Login *</label>
-                    <br />
-                    <input type="text" placeholder="Insira seu login ou e-mail" /> <br />
-                    <label htmlFor="">Senha*</label> <br />
-                    <input type="text" placeholder="insira sua senha" />
-                    <br />
-                    <a href="">Esqueci minha senha</a>
+                    <div className="text">
+                        <div className="primeira">
+                            <h2>Acesse sua conta</h2>
+                            <p>Novo cliente? Então registre-se <a href="">aqui</a></p>
+                        </div>
+
+                        <div className="segunda">
+                            <label htmlFor="">Login *</label>
+                            <br />
+                            <input type="text" placeholder=" Insira seu login ou e-mail" /> <br />
+                            <label htmlFor="">Senha*</label>
+                            <br />
+                            <input type="text" placeholder=" Insira sua senha" />
+                            <br />
+                        </div>
+
+                        <div className="terceira">
+                            <a href="">Esqueci minha senha</a>
+                        </div>
+
+                        <div className="quarta">
+                            <button>Acessar Conta</button>
+                        </div>
+
+                        <div className="icons-login">
+                            <p>Ou faça login com </p>
+                           <div>
+                           <img src={Image3} alt="" />
+                           <img src={Image4} alt="" />
+                           </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div className="sapatos">
@@ -33,7 +58,7 @@ function PaginaLogin() {
                 </div>
             </div>
 
-            <Footer/>
+            <Footer />
 
         </div>
     )
